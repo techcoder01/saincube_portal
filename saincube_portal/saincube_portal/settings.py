33b0b7ff519d0e93882e27731f5dab346eb552e6
Abdul Hannan <121096664+techcoder01@users.noreply.github.com>
2024-07-12 17:26:12 +0500

    fixed

--- saincube_portal/saincube_portal/settings.py
+++ saincube_portal/saincube_portal/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ywom2ip%pc1)0!3*+146ahq)n@_si8p#+&-evf-gwq-we4tuu5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
 
 
 # Application definition
